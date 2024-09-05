bdc20aa039d20c99cc67a569b25769534ea562b7
StillNoNick <nikita@sheverev.com>
2024-09-05 17:36:03 +0300

    Update with generated code

--- src/PageComponent.tsx
+++ src/PageComponent.tsx
@@ -1,11 +1,29 @@
 import { Container } from "./components/Container";
-import { Button } from "./components/Button";
-import { Text } from "./components/Text";
 import { Card, CardTop, CardBottom } from "./components/Card";
+import { Text } from "./components/Text";
+import { Button } from "./components/Button";
 
 export function PageComponent() {
   return (
       <Container background="#f1f6ed" padding="29">
+        <Card background="#8f5555" padding="3">
+          <CardTop>
+            <Text text="Title" fontSize="20"></Text>
+            <Text text="Subtitle" fontSize="15"></Text>
+          </CardTop>
+          <CardBottom>
+            <Button size="small" variant="contained" color="primary" onClickScript="">Learn more</Button>
+          </CardBottom>
+        </Card>
+        <Card background="#de8e8e" padding="3">
+          <CardTop>
+            <Text text="Title" fontSize="20"></Text>
+            <Text text="Subtitle" fontSize="15"></Text>
+          </CardTop>
+          <CardBottom>
+            <Button size="small" variant="contained" color="primary" onClickScript="">Learn more</Button>
+          </CardBottom>
+        </Card>
         <Container background="#ffffff" padding="20">
           <Button size="small" variant="contained" color="primary" onClickScript="alert('i am alive')">Click me</Button>
           <Button size="small" variant="contained" color="primary" onClickScript="">Click me</Button>
